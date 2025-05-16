ce1bc6b36f56d4e792bab48663d179f924feb51f
Nebula S. Nova <jasontheone111@outlook.com>
2025-05-16 17:25:51 -0400

    Update global.hx

--- Multi Global Script Support/data/global.hx
+++ Multi Global Script Support/data/global.hx
@@ -1,12 +1,28 @@
 import funkin.backend.scripting.MultiThreadedScript;
 import funkin.backend.scripting.GlobalScript;
 
+function readSubFolder(folder) {
+	var globalScripts = Paths.getFolderContent(folder).filter((i:String) -> return StringTools.endsWith(i, '.hx'));
+	for (script in globalScripts) {
+		var daScript = new MultiThreadedScript(Paths.script(folder + '/' + script));
+		daScript.call('create');
+		GlobalScript.scripts.add(daScript.script);
+		daScript.call('postCreate');
+	}
+	for (daFolder in Paths.getFolderDirectories(folder+"/", true)) {
+		readSubFolder(daFolder);
+	}
+}
+
 function new() {
-    var globalScripts = Paths.getFolderContent('data/scripts/global').filter((i:String) -> return StringTools.endsWith(i, '.hx'));
+	var globalScripts = Paths.getFolderContent('data/scripts/global').filter((i:String) -> return StringTools.endsWith(i, '.hx'));
 	for (script in globalScripts) {
 		var daScript = new MultiThreadedScript(Paths.script('data/scripts/global/' + script));
 		daScript.call('create');
 		GlobalScript.scripts.add(daScript.script);
 		daScript.call('postCreate');
+	}
+	for (folder in Paths.getFolderDirectories('data/scripts/global/', true)) {
+		readSubFolder(folder);
 	}
 }
